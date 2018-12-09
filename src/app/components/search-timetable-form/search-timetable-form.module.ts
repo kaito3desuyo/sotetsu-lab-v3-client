98added22256cff6b12488f1a74538955a7cd5f9
kaito3desuyo <admin@seapolis.me>
2018-12-09 16:52:40 +0900

    時刻表検索フォームの修正

--- src/app/components/search-timetable-form/search-timetable-form.module.ts
+++ src/app/components/search-timetable-form/search-timetable-form.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { SearchTimetableFormComponent } from './search-timetable-form.component';
 import { AngularMaterialModule } from 'src/app/modules/angular-material/angular-material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { PipesModule } from 'src/app/modules/pipes/pipes.module';
 
 @NgModule({
   imports: [
     AngularMaterialModule,
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    CommonModule
+    CommonModule,
+    PipesModule.forRoot()
   ],
   exports: [SearchTimetableFormComponent],
   declarations: [SearchTimetableFormComponent]
