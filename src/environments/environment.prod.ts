92ccc567972b18bf78c98423ca420c7f05e90b54
kaito3desuyo <admin@seapolis.me>
2019-05-18 20:20:05 +0900

    環境別URLを追加

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.sotetsu-lab.com/',
+  backendUrl: 'https://backend.sotetsu-lab.com/',
+  socketUrl: 'https://api.sotetsu-lab.com/'
 };
