1389126b7cdc83090140b219f67c3306be7d0114
kaito3desuyo <admin@seapolis.me>
2019-05-18 20:56:07 +0900

    アドレス間違えてた

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.sotetsu-lab.com/',
-  backendUrl: 'https://backend.sotetsu-lab.com/',
-  socketUrl: 'https://api.sotetsu-lab.com/'
+  apiUrl: 'https://api.sotetsu-lab.com',
+  backendUrl: 'https://backend.sotetsu-lab.com',
+  socketUrl: 'https://api.sotetsu-lab.com'
 };
