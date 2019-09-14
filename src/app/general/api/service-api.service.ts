8513348e7bcaf85abf09242862b22ce159c22ed9
kaito3desuyo <admin@seapolis.me>
2019-09-14 12:55:15 +0900

    列車種別表示実装 デザイン調整

---
+++ src/app/general/api/service-api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceApiService {
+  private apiUrl = environment.apiUrl + '/v1/services';
+
+  constructor(private http: HttpClient) {}
+
+  searchServices(query: { service_name?: string }): Observable<any> {
+    return this.http.get(this.apiUrl + '/search', {
+      params: query
+    });
+  }
+}
--- src/app/pages/operation/general/services/operation-real-time-resolver.service.ts
+++ src/app/pages/operation/general/services/operation-real-time-resolver.service.ts
@@ -1,102 +1,39 @@
 import { Injectable } from '@angular/core';
 import { OperationRealTimeService } from './operation-real-time.service';
-import { Observable, of } from 'rxjs';
+import { Observable, of, forkJoin } from 'rxjs';
 import { Resolve } from '@angular/router';
-import { CurrentParamsService } from 'src/app/general/models/current-params/current-params.service';
 import { flatMap } from 'rxjs/operators';
-import moment from 'moment';
-import { CurrentParamsQuery } from 'src/app/general/models/current-params/current-params.query';
-import { CalendersService } from 'src/app/general/models/calenders/state/calenders.service';
 
 @Injectable()
-export class OperationRealTimeFormationNumbersResolverService
+export class OperationRealTimeResolverService
   implements Resolve<Observable<void>> {
   constructor(private operationRealTimeService: OperationRealTimeService) {}
 
   resolve(): Observable<void> {
-    return this.operationRealTimeService.fetchFormationNumbers();
-  }
-}
-
-@Injectable()
-export class OperationRealTimeOperationNumbersResolverService
-  implements Resolve<Observable<void>> {
-  constructor(
-    private operationRealTimeService: OperationRealTimeService,
-    private currentParamsQuery: CurrentParamsQuery,
-    private currentParamsService: CurrentParamsService
-  ) {}
-
-  resolve(): Observable<void> {
-    console.log(this.currentParamsQuery.getValue());
-    const currentParams = this.currentParamsQuery.getValue();
-    if (
-      currentParams.calender.id &&
-      currentParams.today === currentParams.calender.lastUpdatedAt
-    ) {
-      return this.operationRealTimeService.fetchOperationNumbers();
-    }
-    return this.currentParamsService
-      .fetchSpecifiedDateCalenderId(
-        moment()
-          .subtract(moment().hour() < 4 ? 1 : 0)
-          .format('YYYY-MM-DD')
-      )
-      .pipe(
-        flatMap(() => {
-          return this.operationRealTimeService.fetchOperationNumbers();
-        })
-      );
-  }
-}
-
-@Injectable()
-export class OperationRealTimeOperationsAllTripsResolverService
-  implements Resolve<Observable<void>> {
-  constructor(private operationRealTimeService: OperationRealTimeService) {}
-
-  resolve(): Observable<void> {
-    return this.operationRealTimeService.fetchOperationTrips();
-  }
-}
-
-@Injectable()
-export class OperationRealTimeStationsResolverService
-  implements Resolve<Observable<void>> {
-  constructor(private operationRealTimeService: OperationRealTimeService) {}
-
-  resolve(): Observable<void> {
-    return this.operationRealTimeService.fetchStations();
-  }
-}
-
-@Injectable()
-export class OperationRealTimeTripsResolverService
-  implements Resolve<Observable<void>> {
-  constructor(
-    private operationRealTimeService: OperationRealTimeService,
-    private currentParamsQuery: CurrentParamsQuery,
-    private currentParamsService: CurrentParamsService
-  ) {}
-
-  resolve(): Observable<void> {
-    const currentParams = this.currentParamsQuery.getValue();
-    if (
-      currentParams.calender.id &&
-      currentParams.today === currentParams.calender.lastUpdatedAt
-    ) {
-      return this.operationRealTimeService.fetchTrips();
-    }
-    return this.currentParamsService
-      .fetchSpecifiedDateCalenderId(
-        moment()
-          .subtract(moment().hour() < 4 ? 1 : 0)
-          .format('YYYY-MM-DD')
-      )
-      .pipe(
-        flatMap(() => {
-          return this.operationRealTimeService.fetchTrips();
-        })
-      );
+    return forkJoin([
+      this.operationRealTimeService.fetchServices(),
+      this.operationRealTimeService.fetchCalenders(),
+      this.operationRealTimeService.fetchStations()
+    ]).pipe(
+      flatMap(() => {
+        const services = this.operationRealTimeService.getServicesAsStatic();
+        const calenders = this.operationRealTimeService.getCalendersAsStatic();
+        return forkJoin([
+          this.operationRealTimeService.fetchFormationNumbers(),
+          this.operationRealTimeService.fetchOperationNumbers(calenders[0].id),
+          this.operationRealTimeService.fetchOperationTrips(calenders[0].id),
+          this.operationRealTimeService.fetchTripClasses(services[0].id)
+        ]);
+      }),
+      flatMap(() => {
+        return forkJoin([
+          this.operationRealTimeService.fetchFormationSightings(),
+          this.operationRealTimeService.fetchOperationSightings()
+        ]);
+      }),
+      flatMap(() => {
+        return of(null);
+      })
+    );
   }
 }
