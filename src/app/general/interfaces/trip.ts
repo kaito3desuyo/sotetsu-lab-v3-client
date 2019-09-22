50b4b7e4ebc1813581de9653bd40b99c1abd5ed0
kaito3desuyo <admin@seapolis.me>
2019-09-22 22:08:21 +0900

    定義ファイル編集

--- src/app/general/interfaces/trip.ts
+++ src/app/general/interfaces/trip.ts
@@ -1,11 +1,11 @@
 import { ITime } from './time';
+import { ITripOperationList } from './trip-operation-list';
 
 export interface ITrip {
   id: string;
   serviceId: string;
   calendarId: string;
   extraCalendarId: string;
-  operationId: string;
   tripNumber: string;
   tripName: string;
   tripDirection: number;
@@ -15,5 +15,6 @@ export interface ITrip {
   depotOut: boolean;
   createdAt: string;
   updatedAt: string;
+  tripOperationLists?: ITripOperationList[];
   times?: ITime[];
 }
