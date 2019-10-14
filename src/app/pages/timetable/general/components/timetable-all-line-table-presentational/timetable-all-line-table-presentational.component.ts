ad39cae1a69a77fc9145a2fede79d91e1da8a6d3
kaito3desuyo <admin@seapolis.me>
2019-10-14 15:26:49 +0900

    時刻表表示の微修正

--- src/app/pages/timetable/general/components/timetable-all-line-table-presentational/timetable-all-line-table-presentational.component.ts
+++ src/app/pages/timetable/general/components/timetable-all-line-table-presentational/timetable-all-line-table-presentational.component.ts
@@ -59,9 +59,16 @@ export class TimetableAllLineTablePresentationalComponent implements OnInit {
     if (time) {
       switch (true) {
         case mode === 'arrival' &&
-          station.viewMode === this.staitonViewMode.DEPARTURE_AND_ARRIVAL &&
-          !time.arrivalTime:
-          if (time.dropoffType === 1) {
+          station.viewMode === this.staitonViewMode.DEPARTURE_AND_ARRIVAL:
+          if (
+            time.pickupType === 1 &&
+            time.dropoffType === 1 &&
+            !time.arrivalTime
+          ) {
+            return '↓';
+          }
+
+          if (!time.arrivalTime) {
             return '‥';
           }
 
@@ -76,25 +83,35 @@ export class TimetableAllLineTablePresentationalComponent implements OnInit {
           }
           return this.formatTime(time.arrivalTime);
         case mode === 'departure' &&
-          (station.viewMode === this.staitonViewMode.ONLY_DEPARTURE ||
-            station.viewMode === this.staitonViewMode.DEPARTURE_AND_ARRIVAL) &&
-          !time.departureTime:
+          station.viewMode === this.staitonViewMode.ONLY_DEPARTURE:
           if (
             time.pickupType === 1 &&
             time.dropoffType === 1 &&
-            !time.arrivalTime
+            !time.departureTime
           ) {
             return '↓';
           }
 
+          if (!time.departureTime) {
+            return this.formatTime(time.arrivalTime);
+          }
+
+          return this.formatTime(time.departureTime);
+        case mode === 'departure' &&
+          station.viewMode === this.staitonViewMode.DEPARTURE_AND_ARRIVAL:
           if (
             time.pickupType === 1 &&
-            station.viewMode === this.staitonViewMode.DEPARTURE_AND_ARRIVAL
+            time.dropoffType === 1 &&
+            !time.departureTime
           ) {
+            return '↓';
+          }
+
+          if (!time.departureTime) {
             return '‥';
           }
 
-          return this.formatTime(time.arrivalTime);
+          return this.formatTime(time.departureTime);
         case mode === 'departure':
           if (
             time.pickupType === 1 &&
@@ -125,6 +142,24 @@ export class TimetableAllLineTablePresentationalComponent implements OnInit {
 
       if (isExistTimeBeforeStation && isExistTimeAfterStation) {
         return '|';
+      }
+
+      const minus1Station = this.stations[stationIndex - 1];
+      if (minus1Station) {
+        const minus1Time = find(trip.times, time => {
+          return time.stationId === minus1Station.id;
+        });
+
+        if (
+          minus1Time &&
+          ((mode === 'arrival' &&
+            station.viewMode === this.staitonViewMode.DEPARTURE_AND_ARRIVAL) ||
+            (mode === 'departure' &&
+              station.viewMode === this.staitonViewMode.ONLY_DEPARTURE &&
+              minus1Station.viewMode === this.staitonViewMode.ONLY_DEPARTURE))
+        ) {
+          return '=';
+        }
       }
 
       return '‥';
