a0fe20f9efe298646f4d828411cd2d7afc2af177
kaito3desuyo <admin@seapolis.me>
2019-11-22 21:39:14 +0900

    自動更新のトグルスイッチを実装する 現在位置表示の入り切りスイッチを実装する

---
+++ src/app/pages/operation/general/components/operation-sightings-control-panel-presentational/operation-sightings-control-panel-presentational.component.ts
@@ -0,0 +1,41 @@
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Output,
+  EventEmitter,
+  Input
+} from '@angular/core';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { Moment } from 'moment';
+
+@Component({
+  selector: 'app-operation-sightings-control-panel-presentational',
+  templateUrl:
+    './operation-sightings-control-panel-presentational.component.html',
+  styleUrls: [
+    './operation-sightings-control-panel-presentational.component.scss'
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class OperationSightingsControlPanelPresentationalComponent {
+  @Input() finalUpdateTime: Moment;
+  @Input() autoReloadEnabled: boolean;
+  @Input() visibleCurrentPosition: boolean;
+  @Output() changeAutoReloadToggle: EventEmitter<boolean> = new EventEmitter<
+    boolean
+  >();
+  @Output() changeVisibleCurrentPositionToggle: EventEmitter<
+    boolean
+  > = new EventEmitter<boolean>();
+
+  constructor() {}
+
+  onChangeAutoReloadToggle(event: MatSlideToggleChange) {
+    this.changeAutoReloadToggle.emit(event.checked);
+  }
+
+  onChangeVisibleCurrentPositionToggle(event: MatSlideToggleChange) {
+    this.changeVisibleCurrentPositionToggle.emit(event.checked);
+  }
+}
