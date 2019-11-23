88fe1faf28c4bfc6afe5bf7733e61e634a7add79
kaito3desuyo <admin@seapolis.me>
2019-11-23 15:27:54 +0900

    APIの向き先を変更

--- src/app/general/general.module.ts
+++ src/app/general/general.module.ts
@@ -1,19 +1,19 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ConfirmDialogContainerComponent } from './components/confirm-dialog-container/confirm-dialog-container.component';
-import { ConfirmDialogPresentationalComponent } from './components/confirm-dialog-presentational/confirm-dialog-presentational.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ErrorHandlerService } from './services/error-handler.service';
-import { AuthInterceptor } from './interceptors/auth-interceptor';
-import { MatPaginatorIntl } from '@angular/material/paginator';
-import { CustomPaginator } from './classes/custom-paginator';
-import { environment } from 'src/environments/environment';
+import { NgModule, ErrorHandler } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ConfirmDialogContainerComponent } from "./components/confirm-dialog-container/confirm-dialog-container.component";
+import { ConfirmDialogPresentationalComponent } from "./components/confirm-dialog-presentational/confirm-dialog-presentational.component";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { LoggerModule, NgxLoggerLevel } from "ngx-logger";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { ErrorHandlerService } from "./services/error-handler.service";
+import { AuthInterceptor } from "./interceptors/auth-interceptor";
+import { MatPaginatorIntl } from "@angular/material/paginator";
+import { CustomPaginator } from "./classes/custom-paginator";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   imports: [
@@ -46,13 +46,15 @@ import { environment } from 'src/environments/environment';
     {
       provide: ErrorHandler,
       useClass: ErrorHandlerService
-    },
+    }
+    /* 
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       // 必須：HTTP_INTERCEPTORSが配列であることを示す
       multi: true
     }
+    */
   ]
 })
 export class GeneralModule {}
