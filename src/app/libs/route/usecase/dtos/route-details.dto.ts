c890552c3e8550f32a9f5f6d43824f636797b036
kaito3desuyo <admin@seapolis.me>
2021-11-23 04:50:39 +0000

    route model/dto/query/service 実装

---
+++ src/app/libs/route/usecase/dtos/route-details.dto.ts
@@ -0,0 +1,45 @@
+import { Expose, Type } from 'class-transformer';
+import { RouteStationListStateStore } from 'src/app/global-states/route-station-list.state';
+import { RouteStationListDetailsDto } from './route-station-list-details.dto';
+
+export class RouteDetailsDto {
+    @Expose({ name: 'id' })
+    routeId: string;
+
+    @Expose()
+    agencyId?: string;
+
+    @Expose()
+    routeNumber?: string;
+
+    @Expose()
+    routeName?: string;
+
+    @Expose()
+    routeNickName?: string;
+
+    @Expose()
+    routeDescription?: string;
+
+    @Expose()
+    routeType?: number;
+
+    @Expose()
+    routeUrl?: string;
+
+    @Expose()
+    routeColor?: string;
+
+    @Expose()
+    routeTextColor?: string;
+
+    @Expose()
+    createdAt?: string;
+
+    @Expose()
+    updatedAt?: string;
+
+    @Expose()
+    @Type(() => RouteStationListDetailsDto)
+    routeStationLists?: RouteStationListStateStore[];
+}
