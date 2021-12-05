801498966f9cca9d769581f83a6550cb7c891214
kaito3desuyo <admin@seapolis.me>
2021-12-05 06:55:34 +0000

    Pipe有効

--- src/app/core/pipes/pipes.module.ts
+++ src/app/core/pipes/pipes.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DayjsPipe } from './dayjs.pipe';
+import { OperationNumberColorPipe } from './operation-number-color.pipe';
+import { FindByIdPipe } from './find-by-id.pipe';
 
 @NgModule({
-    declarations: [DayjsPipe],
+    declarations: [DayjsPipe, OperationNumberColorPipe, FindByIdPipe],
     imports: [CommonModule],
-    exports: [DayjsPipe],
+    exports: [DayjsPipe, OperationNumberColorPipe, FindByIdPipe],
 })
 export class PipesModule {}
