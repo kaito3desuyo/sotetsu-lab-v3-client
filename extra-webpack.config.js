cdd3a66073b895deef60aab22bcf4008364df526
kaito3desuyo <admin@seapolis.me>
2021-12-05 09:51:52 +0000

    ビルド設定更新

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,35 +1,9 @@
-const webpack = require('webpack');
-const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+var MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
 module.exports = {
-  /*
-  optimization: {
-    splitChunks: {
-      chunks: 'async',
-      minSize: 50000,
-      maxSize: 900000,
-      minChunks: 1,
-      maxAsyncRequests: 5,
-      maxInitialRequests: 3,
-      automaticNameDelimiter: '.',
-      name: true,
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true
-        }
-      }
-    }
-  },
-  */
-  plugins: [
-    new MomentLocalesPlugin({
-      localesToKeep: ['ja']
-    })
-  ]
+    plugins: [
+        new MomentLocalesPlugin({
+            localesToKeep: ['ja'],
+        }),
+    ],
 };
