4442b5c668a6ab498b0af9f1d90100537d1e1fd0
kaito3desuyo <admin@seapolis.me>
2022-03-20 06:06:35 +0000

    bug: 運用検索フォームとRouterとの紐づけがなかったので追加

--- src/app/pages/operation/operation-past-time/operation-past-time.component.ts
+++ src/app/pages/operation/operation-past-time/operation-past-time.component.ts
@@ -1,23 +1,44 @@
-import { Component, OnInit, Inject, Injector } from '@angular/core';
-import { BaseComponent } from 'src/app/general/classes/base-component';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RxState } from '@rx-angular/state';
 import { TitleService } from 'src/app/general/services/title.service';
+import { OperationSearchCardService } from 'src/app/shared/operation-search-card/services/operation-search-card.service';
 
 @Component({
     selector: 'app-operation-past-time',
     templateUrl: './operation-past-time.component.html',
     styleUrls: ['./operation-past-time.component.scss'],
+    providers: [RxState],
 })
-export class OperationPastTimeComponent extends BaseComponent {
+export class OperationPastTimeComponent {
     constructor(
-        @Inject(Injector) injector: Injector,
-        private route: ActivatedRoute,
-        private titleService: TitleService
+        private readonly route: ActivatedRoute,
+        private readonly router: Router,
+        private readonly state: RxState<{}>,
+        private readonly titleService: TitleService,
+        private readonly operationSearchCardService: OperationSearchCardService
     ) {
-        super(injector);
-        this.subscription = this.route.data.subscribe(
-            (data: { title: string }) => {
-                this.titleService.setTitle(data.title);
+        this.state.hold(this.route.data, (data: { title: string }) => {
+            this.titleService.setTitle(data.title);
+        });
+
+        this.state.hold(
+            this.operationSearchCardService.receiveSearchOperationTableEvent(),
+            (calendarId) => {
+                this.router.navigate([
+                    '/operation/table',
+                    { calendar_id: calendarId },
+                ]);
+            }
+        );
+
+        this.state.hold(
+            this.operationSearchCardService.receiveSearchOperationRouteDiagramEvent(),
+            (operationId) => {
+                this.router.navigate([
+                    '/operation/route-diagram',
+                    { operation_id: operationId },
+                ]);
             }
         );
     }
