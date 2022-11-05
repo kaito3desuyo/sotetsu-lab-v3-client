859a3bd03a56a076912ef2051262dbd3d1f0ab8e
kaito3desuyo <admin@seapolis.me>
2022-11-05 11:13:24 +0000

    refactor: :wastebasket: 整理

--- src/app/pages/operation/operation-real-time/operation-real-time.module.ts
+++ src/app/pages/operation/operation-real-time/operation-real-time.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -40,7 +39,6 @@ import {
         MatTableModule,
         MatIconModule,
         MatFormFieldModule,
-        MatDatepickerModule,
         MatSortModule,
         AdsenseModule,
         PipesModule,
