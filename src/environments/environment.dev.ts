17f316ed5e6e927a64774f9bc1af6140b59d2e10
kaito3desuyo <admin@seapolis.me>
2022-11-21 22:53:56 +0900

    refactor: :chart_with_upwards_trend: GA4に移行

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,6 +5,6 @@ export const environment = {
     socketUrl: 'wss://lxac6g1ue7.execute-api.ap-northeast-1.amazonaws.com/Prod',
     clientId: '43t7n2as7aahnqr1vbu9de4ds9',
     analytics: {
-        id: 'UA-38754264-6', // IDを設定する、environment.prod.tsにも
+        id: 'G-CFKSDNMCY3', // IDを設定する、environment.prod.tsにも
     },
 };
