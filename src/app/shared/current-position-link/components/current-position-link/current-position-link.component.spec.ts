e6fd767299bd8aa9e8996dfd74688d79d5e3f1bd
kaito3desuyo <admin@seapolis.me>
2023-06-16 13:55:16 +0000

    feat: :sparkles: 現在位置リンクコンポーネントの実装

---
+++ src/app/shared/current-position-link/components/current-position-link/current-position-link.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CurrentPositionLinkComponent } from './current-position-link.component';
+
+describe('CurrentPositionLinkComponent', () => {
+  let component: CurrentPositionLinkComponent;
+  let fixture: ComponentFixture<CurrentPositionLinkComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ CurrentPositionLinkComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CurrentPositionLinkComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
