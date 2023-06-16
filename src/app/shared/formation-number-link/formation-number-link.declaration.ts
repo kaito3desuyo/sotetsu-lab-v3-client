fcf90f98e90c93396c645b004bfd6004a76e77ab
kaito3desuyo <admin@seapolis.me>
2023-06-16 13:33:10 +0000

    feat: :sparkles: 編成番号リンクコンポーネントの実装

---
+++ src/app/shared/formation-number-link/formation-number-link.declaration.ts
@@ -0,0 +1,7 @@
+import { FormationNumberLinkComponent } from './components/formation-number-link/formation-number-link.component';
+
+const FORMATION_NUMBER_LINK_DECLARATIONS = [
+    FormationNumberLinkComponent,
+] as const;
+
+export default FORMATION_NUMBER_LINK_DECLARATIONS;
