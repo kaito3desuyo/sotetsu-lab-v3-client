ec0b055718a722451e29fe70397709377bf1ea08
kaito3desuyo <admin@seapolis.me>
2024-03-01 20:49:39 +0900

    refactor: :alien: WebSocket APIのURLを移行する

--- src/app/core/services/socket.service.ts
+++ src/app/core/services/socket.service.ts
@@ -1,34 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { NGXLogger } from 'ngx-logger';
 import { Observable, Subject } from 'rxjs';
+import { TokenStateQuery } from 'src/app/global-states/token.state';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root',
 })
 export class SocketService {
+    private readonly _tokenStateQuery = inject(TokenStateQuery);
+    private readonly _url = environment.socketUrl;
     private readonly _ev$ = new Subject<unknown>();
-    private readonly url = environment.socketUrl;
-    private conn: WebSocket;
+    private _conn: WebSocket;
 
     constructor(private readonly logger: NGXLogger) {}
 
     connect(): void {
-        const conn = new WebSocket(this.url);
-        this.conn = conn;
-        this.conn.onmessage = (ev: MessageEvent<unknown>) => {
+        this._conn = new WebSocket(
+            `${this._url}?token=${this._tokenStateQuery.accessToken}`
+        );
+        this._conn.onmessage = (ev: MessageEvent<unknown>) => {
             this._ev$.next(ev);
         };
         this.logger.log('SocketService: Connected to WebSocket server');
     }
 
     disconnect(): void {
-        this.conn.close();
+        this._conn.close();
         this.logger.log('SocketService: Disconnected to WebSocket server');
     }
 
     emit(action: string, data: unknown): void {
-        this.conn.send(
+        this._conn.send(
             JSON.stringify({
                 action,
                 data: JSON.stringify(data),
