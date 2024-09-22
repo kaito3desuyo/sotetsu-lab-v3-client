15bcd7aa2207b458a92c95e23a8ca2d4f76f3ab0
kaito3desuyo <admin@seapolis.me>
2024-09-22 05:52:54 +0000

    refactor: :recycle: 時刻表情報投稿カードをリファクタリングする

--- src/app/shared/timetable-post-card/components/timetable-post-card-c/timetable-post-card-c.component.ts
+++ src/app/shared/timetable-post-card/components/timetable-post-card-c/timetable-post-card-c.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { CalendarListStateQuery } from 'src/app/global-states/calendar-list.state';
 import { ITimetablePostCardForm } from '../../interfaces/timetable-post-card-form.interface';
 import { TimetablePostCardService } from '../../services/timetable-post-card.service';
@@ -10,17 +10,16 @@ import { TimetablePostCardPComponent } from '../timetable-post-card-p/timetable-
     selector: 'app-timetable-post-card-c',
     templateUrl: './timetable-post-card-c.component.html',
     styleUrls: ['./timetable-post-card-c.component.scss'],
-    imports: [CommonModule, TimetablePostCardPComponent],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [TimetablePostCardPComponent],
 })
 export class TimetablePostCardCComponent {
-    private readonly calendarListStateQuery = inject(CalendarListStateQuery);
-    private readonly timetablePostCardService = inject(
-        TimetablePostCardService,
-    );
+    readonly #calendarListStateQuery = inject(CalendarListStateQuery);
+    readonly #timetablePostCardService = inject(TimetablePostCardService);
 
-    readonly calendars$ = this.calendarListStateQuery.calendars$;
+    readonly calendars = toSignal(this.#calendarListStateQuery.calendars$);
 
     onReceiveClickMoveTimetableAdd(state: ITimetablePostCardForm): void {
-        this.timetablePostCardService.emitMoveTimetableAddEvent(state);
+        this.#timetablePostCardService.emitMoveTimetableAddEvent(state);
     }
 }
